3c3d90d8d72aa1acf5df43c796dc21dcb6ad1cab
Gianluca Rijo Jumelles <gianlucarijo99@gmail.com>
2026-05-12 15:22:36 +0200

    feat: add register, login, packages and booking routes with bcrypt password hashing

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import bodyParser from "body-parser";
 import pg from "pg";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import bcrypt, { hash } from "bcrypt";
 
 
 const app = express();
@@ -28,7 +28,8 @@ const db = new pg.Client({
 db.connect();
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("../frontend/public"));
 
 // get route homepage
@@ -39,6 +40,92 @@ app.get("/", async (req, res) => {
     res.render("index.ejs", {messages: helloWorld});
 });
 
+// get route register
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+// get route pacchetti 
+app.get("/pacchetti", (req, res) => {
+    res.render("pacchetti.ejs"); // DA MODIFICARE
+});
+
+// get route bookings:id 
+app.get("/booking/:id", async (req, res) => {
+    try {
+        const result = await db.query(
+            "SELECT * FROM utente WHERE id = $1",
+            [req.params.id]
+        );
+
+        if (result.rows.length > 0) {
+            res.render("booking.ejs", {name: result.rows[0].nome})
+        } else {
+            res.status(404).send("prenotazione non trovata");
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+// post register
+app.post("/register", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const checkResult = await db.query("SELECT * FROM utente WHERE email = $1",
+            [email]
+        );
+
+        if (checkResult.rows.length > 0) {
+            res.send("Email alredy exist. Try loggin in.");
+        } else {
+            // Password Hashing
+          bcrypt.hash(password, saltRound, async(err, hash) => {
+            if (err) {
+            console.log("Error hashing password:", err);
+            } else {
+            await db.query(
+                "INSERT INTO utente (email, password) VALUES ($1, $2)",
+                [email, hash]
+            );
+            res.render("register.ejs");
+            }
+        })
+    }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// post route /login
+app.post("/login", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+      const result = await db.query("SELECT * FROM utente WHERE email = $1",
+        [email]
+      );
+
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        console.log(user);
+        const storedPassword = user.password;
+
+        if (password === storedPassword) {
+            res.render("index.ejs"); // DA CAMBIARE!!!
+        } else {
+            res.render("register.ejs");  // DA CAMBIARE!!!
+        }
+      }
+    } catch (err) {
+        console.log(err);
+    }
+})
 
 
 app.listen(port, () => {
